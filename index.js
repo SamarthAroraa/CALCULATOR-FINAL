6c35d9c26a4f808e33eb3cc0e5c8488f637f8881
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-07-19 07:20:13 +0530

    port setting changed according to deployment standards

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
+const port =  process.env.PORT || 3031;
 
 app.get("/", function (req, res) {
   return res.sendFile(path.join(__dirname + "/index.html"));
 });
 app.use(express.static("./"));
 
-const port = 8000;
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error running server: ${err}`);
